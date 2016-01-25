7b76e995220c48235f001f8c5632f0047d90f5f1
Mikhail Onyshchuk <onyshchukms@gmail.com>
2016-01-25 19:03:30 +0200

    Initial commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://fruit.loc:8888/admin/');
+define('HTTP_CATALOG', 'http://fruit.loc:8888/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://fruit.loc:8888/admin/');
+define('HTTPS_CATALOG', 'http://fruit.loc:8888/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/misha/Workspace/php_projects/fruit/admin/');
+define('DIR_SYSTEM', '/Users/misha/Workspace/php_projects/fruit/system/');
+define('DIR_LANGUAGE', '/Users/misha/Workspace/php_projects/fruit/admin/language/');
+define('DIR_TEMPLATE', '/Users/misha/Workspace/php_projects/fruit/admin/view/template/');
+define('DIR_CONFIG', '/Users/misha/Workspace/php_projects/fruit/system/config/');
+define('DIR_IMAGE', '/Users/misha/Workspace/php_projects/fruit/image/');
+define('DIR_CACHE', '/Users/misha/Workspace/php_projects/fruit/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/misha/Workspace/php_projects/fruit/system/storage/download/');
+define('DIR_LOGS', '/Users/misha/Workspace/php_projects/fruit/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/misha/Workspace/php_projects/fruit/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/misha/Workspace/php_projects/fruit/system/storage/upload/');
+define('DIR_CATALOG', '/Users/misha/Workspace/php_projects/fruit/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'fruit');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://fruit.loc:8888/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://fruit.loc:8888/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/misha/Workspace/php_projects/fruit/catalog/');
+define('DIR_SYSTEM', '/Users/misha/Workspace/php_projects/fruit/system/');
+define('DIR_LANGUAGE', '/Users/misha/Workspace/php_projects/fruit/catalog/language/');
+define('DIR_TEMPLATE', '/Users/misha/Workspace/php_projects/fruit/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/misha/Workspace/php_projects/fruit/system/config/');
+define('DIR_IMAGE', '/Users/misha/Workspace/php_projects/fruit/image/');
+define('DIR_CACHE', '/Users/misha/Workspace/php_projects/fruit/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/misha/Workspace/php_projects/fruit/system/storage/download/');
+define('DIR_LOGS', '/Users/misha/Workspace/php_projects/fruit/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/misha/Workspace/php_projects/fruit/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/misha/Workspace/php_projects/fruit/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'fruit');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
